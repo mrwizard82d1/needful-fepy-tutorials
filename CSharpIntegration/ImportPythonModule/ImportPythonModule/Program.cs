0da17d6387cbf6a920a9371412ba4e791ec4e55d
Larry Jones <larry.jones@reveal-energy.com>
2019-05-19 16:23:32 -0500

    Import and execute script functions

---
+++ CSharpIntegration/ImportPythonModule/ImportPythonModule/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using IronPython.Hosting;
+
+namespace ImportPythonModule
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var engine = Python.CreateEngine();
+
+            var searchPaths = engine.GetSearchPaths();
+            searchPaths.Add("./ScriptPlugins");
+            engine.SetSearchPaths(searchPaths);
+
+            var scope = engine.ImportModule("interesting_stuff");
+
+            var printHelloWorldFunction = scope.GetVariable("print_hello_world");
+            printHelloWorldFunction();
+
+            var printMessageFunction = scope.GetVariable("print_message");
+            printMessageFunction("Goodbye!");
+
+            var addFunction = scope.GetVariable("add");
+            Console.WriteLine($"The sum of 1 and 2 is {addFunction(1, 2)}.");
+        }
+    }
+}
---
+++ CSharpIntegration/ImportPythonModule/ImportPythonModule/ScriptPlugins/interesting_stuff.py
@@ -0,0 +1,8 @@
+def print_hello_world():
+    print('Hello, Iron Python Import Module World')
+    
+def print_message(message):
+    print(message)
+    
+def add(x, y):
+    return x + y
