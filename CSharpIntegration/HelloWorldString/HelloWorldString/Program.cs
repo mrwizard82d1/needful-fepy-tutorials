57198b764314384d512304770cff2ed0690c4324
Larry Jones <larry.jones@reveal-energy.com>
2019-05-19 08:28:46 -0500

    Hello World - from a string

---
+++ CSharpIntegration/HelloWorldString/HelloWorldString/Program.cs
@@ -0,0 +1,14 @@
+﻿using IronPython.Hosting;
+
+namespace HelloWorldString
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var engine = Python.CreateEngine();
+            var script = engine.CreateScriptSourceFromString("print('Hello, IronPython Scripting World!')");
+            script.Execute();
+        }
+    }
+}
